92b67026580a954334ecc18e06ef85e4400f9c9d
Kiffin Gish <kiffin.gish@planet.nl>
2022-12-01 17:04:03 +0100

    Initial setup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Papa as CSV, ParseConfig, ParseResult } from 'ngx-papaparse';
+import { HttpClient } from '@angular/common/http';
+
+const cn = 'AppComponent';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'csv-to-material-table';
+export class AppComponent implements OnInit {
+
+    displayedColumns: string[] = [];
+    dataSource: Array<object> = [];
+
+    constructor(private http: HttpClient, private csv: CSV) {
+    }
+
+    ngOnInit() {
+        this.http.get('assets/data.csv', { responseType: 'text' })
+            .subscribe(data => this.handleData(data));
+    }
+
+    handleData(data: string) {
+        const options: ParseConfig = {
+            complete: results => this.handleComplete(results),
+            delimiter: ',',
+            header: true
+        };
+
+        this.csv.parse(data, options);
+    }
+
+    handleComplete(results: ParseResult<any>) {
+        const { data, errors } = results;
+
+        if (data && data.length > 0) {
+            const badErrors = errors.filter(error => error.type !== 'FieldMismatch');
+            if (badErrors.length) {
+                console.error(`${ cn } handleComplete() errors='${ JSON.stringify(badErrors) }'!`);
+            } else {
+                this.displayedColumns = Object.keys(data[0]);
+                console.log(`${ cn } handleComplete() displayedColumns='${JSON.stringify(this.displayedColumns)}'`);
+                this.dataSource = data;
+            }
+        } else {
+            console.error(`${ cn } handleComplete() empty data!`);
+        }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [];
 
 @NgModule({
   declarations: [
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
